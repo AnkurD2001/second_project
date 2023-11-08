ed0abb9d68488ee26d6963f0ee580d2c33855b00
AnkurD2001 <ankur98amg@gmail.com>
2023-11-08 22:41:36 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def silk(request):
+    return HttpResponse('we are not talking about silk')
